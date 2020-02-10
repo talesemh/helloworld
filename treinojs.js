a2d58341a6dc28500a94d5adf75d9a956a5bce3e
talesemh <taleshcsouza@gmail.com>
2020-02-10 00:13:13 -0300

    Criação do arquivo treinojs.js

---
+++ treinojs.js
@@ -0,0 +1,32 @@
+console.log('Atributo')
+let atributo = 'def';
+console.log(atributo);
+console.log('Levelup')
+let levelup = true;
+console.log(levelup);
+console.log('Level')
+let level = 5;
+console.log(level);
+
+atributo = "atk"
+levelup = false
+level = 10
+console.log('Atributo')
+console.log(atributo)
+console.log('Levelup')
+console.log(levelup)
+console.log('Level')
+console.log(level)
+
+console.log('Personagem')
+const personagem = 'Jão o bárbaro'
+console.log(personagem)
+console.log('Classe')
+const barbaro = true
+console.log(barbaro)
+console.log('Idade')
+const idade = 50
+console.log(idade)
+
+console.log(atributo, levelup, level, personagem, barbaro, idade)
+
